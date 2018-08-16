b5224a71e1b719df3ed719e62f1ab2c6d029bfb4
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-08-16 13:36:20 -0700

    fixing python 2.7 build

--- slicedimage/backends/_caching.py
+++ slicedimage/backends/_caching.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import io
 
 from diskcache import Cache
-from six import BytesIO
+from io import BytesIO
 
 from ._base import Backend
 
