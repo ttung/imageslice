e0287a2f6ef4d2403ff2600eab4ed60467fcab41
Shannon Axelrod <shannon.axelrod@chanzuckerberg.com>
2018-08-16 16:38:24 -0700

    pulling out reused test functionality into seperate files to be used later

---
+++ tests/utils/utility.py
@@ -0,0 +1,31 @@
+import contextlib
+import socket
+
+
+def unused_tcp_port():
+    """
+    Return an unused TCP port.
+    """
+    with contextlib.closing(socket.socket()) as sock:
+        sock.bind(('127.0.0.1', 0))
+        return sock.getsockname()[1]
+
+
+def build_skeleton_manifest():
+    """
+    Returns a 0.0.0 formatted manifest with no tiles.
+    """
+    return {
+        "version": "0.0.0",
+        "dimensions": [
+            "x",
+            "y",
+            "hyb",
+            "ch"
+        ],
+        "shape": {
+            "hyb": 1,
+            "ch": 1
+        },
+        "tiles": []
+    }
