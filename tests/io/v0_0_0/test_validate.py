b114632a56a63ffa80e939ba296a427f4169bb1f
jmoore <josh@glencoesoftware.com>
2018-09-11 12:38:45 +0100

    Add corrupt checksum

--- tests/io/v0_0_0/test_validate.py
+++ tests/io/v0_0_0/test_validate.py
@@ -1,5 +1,6 @@
 import collections
 import os
+import pytest
 import unittest
 
 import slicedimage
@@ -9,8 +10,17 @@ baseurl = "file://{}".format(os.path.abspath(os.path.dirname(__file__)))
 
 
 class TestValidate(unittest.TestCase):
-    def test_checksums(self):
+
+    def test_checksums_good(self):
         result = slicedimage.Reader.parse_doc("hybridization-fov_000.json", baseurl)
         self.assertIsInstance(result, slicedimage.TileSet)
         self.assertEqual(result.shape, {'c': 1, 'r': 1, 'z': 1})
         result.validate()
+
+    def test_checksums_bad(self):
+        result = slicedimage.Reader.parse_doc("hybridization-fov_000.broken", baseurl)
+        self.assertIsInstance(result, slicedimage.TileSet)
+        self.assertEqual(result.shape, {'c': 1, 'r': 1, 'z': 1})
+        # FIXME: backend is complaining on _load before we get to the assert
+        with pytest.raises(slicedimage.backends._base.ChecksumValidationError):
+            result.validate()
